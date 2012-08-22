1dc2c863e4275b7b0191bbccff8b340c2243d0db
Jason Urton <jason.urton@gmail.com>
2012-08-22 10:20:40 -0400

    fix fill queue rake

--- app/models/publication_queue.rb
+++ app/models/publication_queue.rb
@@ -10,7 +10,7 @@ class PublicationQueue < ActiveRecord::Base
         :asker_id => asker.id, 
         :publication_queue_id => queue.id
       )
-      queue.publications << publication.update_attribute(:url, Post.shorten_url("#{URL}/feeds/#{asker.id}/#{publication.id}", "app", "cp", asker.twi_screen_name, question.id))
+      publication.update_attribute(:url, Post.shorten_url("#{URL}/feeds/#{asker.id}/#{publication.id}", "app", "cp", asker.twi_screen_name, question.id))
     end
   end
 
