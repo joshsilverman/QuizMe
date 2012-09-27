50238e38995481f12ef7ad35eb9dde17cf0048e2
Josh Silverman <joshsilverman@Joshs-MacBook-Air.local>
2012-09-26 23:35:37 -0400

    spam filter implemented - truthset loaded onrake

--- db/schema.rb
+++ db/schema.rb
@@ -37,22 +37,22 @@ ActiveRecord::Schema.define(:version => 20120924002526) do
   end
 
   create_table "posts", :force => true do |t|
-    t.integer   "user_id"
-    t.string    "provider"
-    t.text      "text"
-    t.string    "engagement_type"
-    t.string    "provider_post_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "in_reply_to_post_id"
-    t.integer   "publication_id"
-    t.integer   "conversation_id"
-    t.boolean   "responded_to",        :default => false
-    t.integer   "in_reply_to_user_id"
-    t.boolean   "posted_via_app"
-    t.string    "url"
-    t.boolean   "spam"
-    t.boolean   "autospam"
+    t.integer  "user_id"
+    t.string   "provider"
+    t.text     "text"
+    t.string   "engagement_type"
+    t.string   "provider_post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "in_reply_to_post_id"
+    t.integer  "publication_id"
+    t.integer  "conversation_id"
+    t.boolean  "responded_to",        :default => false
+    t.integer  "in_reply_to_user_id"
+    t.boolean  "posted_via_app"
+    t.string   "url"
+    t.boolean  "spam"
+    t.boolean  "autospam"
   end
 
   create_table "publication_queues", :force => true do |t|
