5930f0e143a96f1d3a48b785957d8bb415feb6c5
Josh Silverman <joshsilverman@Joshs-MacBook-Air.local>
2012-10-15 21:58:58 -0400

    include type 2 spam exemption in reports

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,7 +4,7 @@ class ClientsController < ApplicationController
   def report
     @user = User.find params[:id]
     @posts = Post.joins(:user)\
-        .where("in_reply_to_user_id = ? AND users.role != 'asker' AND (spam = false OR (spam IS NULL and autospam = false) OR interaction_type = 3) AND user_id NOT IN (1,3,4,5,11,12,13,17,25,65,106)", params[:id])\
+        .where("in_reply_to_user_id = ? AND users.role != 'asker' AND (spam = false OR (spam IS NULL and autospam = false) OR interaction_type IN (2,3)) AND user_id NOT IN (1,3,4,5,11,12,13,17,25,65,106)", params[:id])\
         .select([:text, "posts.created_at", :in_reply_to_user_id, :twi_screen_name, :interaction_type, :correct, :user_id, :spam, :autospam, "users.role", "users.twi_screen_name"])\
         .order("posts.created_at DESC")
     @posts_by_week = @posts.group_by{|p| p.created_at.strftime('%W')}
