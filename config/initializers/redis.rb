623180cf1db7f8c5bd0ecc3dd10ada096c9183cb
Jason Urton <jason.urton@gmail.com>
2012-11-19 20:01:05 -0500

    check env when setting redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,6 @@
-uri = URI.parse(ENV['REDISTOGO_URL'])
-# uri = URI.parse('redis://localhost:6379')
+if Rails.env.production?
+	uri = URI.parse(ENV['REDISTOGO_URL'])
+else
+	uri = URI.parse('redis://localhost:6379')
+end
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
