bea1a20846a32025f7f7000c410a629e77747c51
Jason Urton <jason.urton@gmail.com>
2012-11-20 18:07:08 -0500

    set redis url only if not nil

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,5 +4,7 @@
 # else
 # 	uri = URI.parse('redis://localhost:6379')
 # end
-uri = URI.parse(ENV['REDISTOGO_URL'])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
+if ENV['REDISTOGO_URL'].present?
+	uri = URI.parse(ENV['REDISTOGO_URL'])
+	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
+end
