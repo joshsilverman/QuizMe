8278341c0c59a2221377916d7c4084036a65d9a7
Jason Urton <jason.urton@gmail.com>
2012-11-20 18:13:42 -0500

    default val for REDIS

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,7 +4,8 @@
 # else
 # 	uri = URI.parse('redis://localhost:6379')
 # end
+REDIS = nil
 if ENV['REDISTOGO_URL'].present?
 	uri = URI.parse(ENV['REDISTOGO_URL'])
 	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
-end
+else
