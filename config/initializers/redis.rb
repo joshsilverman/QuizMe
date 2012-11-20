7a5b930b6837f596bd74ca2ab2579f5aaec24352
Jason Urton <jason.urton@gmail.com>
2012-11-20 18:15:22 -0500

    syntax fix :)

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -8,4 +8,4 @@ REDIS = nil
 if ENV['REDISTOGO_URL'].present?
 	uri = URI.parse(ENV['REDISTOGO_URL'])
 	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
-else
+end
