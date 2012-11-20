d5f868479450827116f8137d6ef636717ff69b47
Jason Urton <jason.urton@gmail.com>
2012-11-19 20:10:46 -0500

    revert

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,8 @@
-if Rails.env.production?
-	uri = URI.parse(ENV['REDISTOGO_URL'])
-else
-	uri = URI.parse('redis://localhost:6379')
-end
+# puts ENV["REDISTOGO_URL"]
+# if Rails.env.production?
+# 	uri = URI.parse(ENV['REDISTOGO_URL'])
+# else
+# 	uri = URI.parse('redis://localhost:6379')
+# end
+uri = URI.parse(ENV['REDISTOGO_URL'])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
