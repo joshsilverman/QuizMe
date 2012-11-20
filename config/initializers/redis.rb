f54edaa44295d22fa1f5d9d0583ea820ce9c5a71
Jason Urton <jason.urton@gmail.com>
2012-11-19 19:43:27 -0500

    rever redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-# uri = URI.parse(ENV['REDISTOGO_URL'])
-uri = URI.parse('redis://localhost:6379')
+uri = URI.parse(ENV['REDISTOGO_URL'])
+# uri = URI.parse('redis://localhost:6379')
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
