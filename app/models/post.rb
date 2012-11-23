60aacc6846d501f3f9d6afc19f5c471e5e63c91c
Jason Urton <jason.urton@gmail.com>
2012-11-23 11:26:46 -0500

    default value for collect rts

--- app/models/post.rb
+++ app/models/post.rb
@@ -273,9 +273,9 @@ class Post < ActiveRecord::Base
     last_post = Post.where("provider like ? and provider_post_id is not null and user_id not in (?) and posted_via_app = ?", 'twitter', asker_ids, false,).order('created_at DESC').limit(1).last
     last_dm = Post.where("provider like ? and provider_post_id is not null and user_id not in (?) and posted_via_app = ?", 'twitter', asker_ids, false).order('created_at DESC').limit(1).last
     client = current_acct.twitter
-    mentions = Post.twitter_request { client.mentions({:count => 50, :since_id => last_post.nil? ? nil : last_post.provider_post_id.to_i}) }
-    retweets = Post.twitter_request { client.retweets_of_me({:count => 50}) }
-    dms = Post.twitter_request { client.direct_messages({:count => 50, :since_id => last_dm.nil? ? nil : last_dm.provider_post_id.to_i}) }
+    mentions = Post.twitter_request { client.mentions({:count => 50, :since_id => last_post.nil? ? nil : last_post.provider_post_id.to_i}) } || []
+    retweets = Post.twitter_request { client.retweets_of_me({:count => 50}) } || []
+    dms = Post.twitter_request { client.direct_messages({:count => 50, :since_id => last_dm.nil? ? nil : last_dm.provider_post_id.to_i}) } || []
     mentions.each { |m| Post.save_mention_data(m, current_acct) }
     retweets.each { |r| Post.save_retweet_data(r, current_acct) }
     dms.each { |d| Post.save_dm_data(d, current_acct) }
