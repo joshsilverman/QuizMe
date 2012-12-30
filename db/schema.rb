abe4147ddcc33ec893f3c18caaffdcffe63aae6a
Josh Silverman <joshs.silverman@gmail.com>
2012-12-30 00:11:19 -0500

    tag model for storing labels for posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121229215246) do
+ActiveRecord::Schema.define(:version => 20121230041045) do
 
   create_table "answers", :force => true do |t|
     t.boolean  "correct"
@@ -161,6 +161,13 @@ ActiveRecord::Schema.define(:version => 20121229215246) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "click_throughs",     :default => 0
+  end
+
+  create_table "tags", :force => true do |t|
+    t.integer  "post_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "topics", :force => true do |t|
