e748ebe58c566d86ea240b6f2a7e3dbaa4e9f46d
Josh Silverman <joshs.silverman@gmail.com>
2013-02-05 15:43:51 -0500

    comment out custom classifier return

--- lib/classifiers/custom_bayes.rb
+++ lib/classifiers/custom_bayes.rb
@@ -1,44 +1,44 @@
-class CustomBayes < StuffClassifier::Bayes
+# class CustomBayes < StuffClassifier::Bayes
 
-  def classify(text, default=nil)
-    puts 'here'
+#   def classify(text, default=nil)
+#     puts 'here'
 
-    # Find the category with the highest probability
-    max_prob = @min_prob
-    best = nil
+#     # Find the category with the highest probability
+#     max_prob = @min_prob
+#     best = nil
 
-    scores = cat_scores(text)
-    scores.each do |score|
-      cat, prob = score
-      if prob > max_prob
-        max_prob = prob
-        best = cat
-      end
+#     scores = cat_scores(text)
+#     scores.each do |score|
+#       cat, prob = score
+#       if prob > max_prob
+#         max_prob = prob
+#         best = cat
+#       end
 
-      puts '-----'
-      puts text
-      puts cat
-      puts prob
-      puts ''
-    end
+#       puts '-----'
+#       puts text
+#       puts cat
+#       puts prob
+#       puts ''
+#     end
     
-    # Return the default category in case the threshold condition was
-    # not met. For example, if the threshold for :spam is 1.2
-    #
-    #    :spam => 0.73, :ham => 0.40  (OK)
-    #    :spam => 0.80, :ham => 0.70  (Fail, :ham is too close)
+#     # Return the default category in case the threshold condition was
+#     # not met. For example, if the threshold for :spam is 1.2
+#     #
+#     #    :spam => 0.73, :ham => 0.40  (OK)
+#     #    :spam => 0.80, :ham => 0.70  (Fail, :ham is too close)
 
-    return default unless best
+#     return default unless best
 
-    threshold = @thresholds[best] || 1.0
+#     threshold = @thresholds[best] || 1.0
 
-    scores.each do |score|
-      cat, prob = score
-      next if cat == best
-      return default if prob * threshold > max_prob
-    end
+#     scores.each do |score|
+#       cat, prob = score
+#       next if cat == best
+#       return default if prob * threshold > max_prob
+#     end
 
-    return 12234    
-  end
+#     return 12234    
+#   end
 
-end
+# end
