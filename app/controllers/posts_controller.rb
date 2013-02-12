fbb5caef3620da6f3e87b350a42fd1f6c0e4325f
Jason Urton <jason.urton@gmail.com>
2013-02-12 17:06:24 -0500

    add rt back in w/ mp event for tracking

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,23 +4,27 @@ class PostsController < ApplicationController
 	def retweet
 		if params[:publication_id]
 			post = Publication.find(params[:publication_id]).posts.last
-			retweet_post = Post.create({
-				:user_id => current_user.id,
-				:provider => "twitter",
-				:in_reply_to_post_id => post.id, 
-				:in_reply_to_user_id => post.user_id,
-				:posted_via_app => true, 
-				:interaction_type => 3
-			})
-
-      current_user.segment
-      current_user.update_user_interactions({
-        :learner_level => "share", 
-        :last_interaction_at => retweet_post.created_at
-      })      
-      
       retweet = Post.twitter_request { current_user.twitter.retweet(post.provider_post_id) }
-			render :json => retweet
+
+      if retweet
+        retweet_post = Post.create({
+          :user_id => current_user.id,
+          :provider => "twitter",
+          :in_reply_to_post_id => post.id, 
+          :in_reply_to_user_id => post.user_id,
+          :posted_via_app => true, 
+          :interaction_type => 3
+        })
+
+        current_user.segment
+        current_user.update_user_interactions({
+          :learner_level => "share", 
+          :last_interaction_at => retweet_post.created_at
+        })      
+        
+        Mixpanel.track_event "in app retweet", { :distinct_id => current_user.id }  
+  			render :json => retweet
+      end
 		else
 			post = Post.find(params[:post_id])
 			asker = Asker.find(params[:asker_id])
