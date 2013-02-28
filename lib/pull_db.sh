a25f1ca517a07331f7b6cc9c754cc1c117ef0e40
Josh Silverman <joshs.silverman@gmail.com>
2013-02-28 06:09:57 -0500

    erase manual backup

--- lib/pull_db.sh
+++ lib/pull_db.sh
@@ -6,14 +6,14 @@ function LastBackupName () {
 
 heroku pgbackups -a wisr >&2
 
-old_backup=$(LastBackupName)
-heroku pgbackups:destroy -a wisr $old_backup >&2
-
 heroku pgbackups:capture -a wisr >&2
 new_backup=$(LastBackupName)
 curl $(heroku pgbackups:url -a wisr $new_backup) > temporary_backup.dump
 
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U wisr -d wisr temporary_backup.dump 
 rm -f temporary_backup.dump
+
+old_backup=$(LastBackupName)
+heroku pgbackups:destroy -a wisr $old_backup >&2
 
 heroku pgbackups -a wisr >&2
