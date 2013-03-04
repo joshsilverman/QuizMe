2e5733010b9349f98153e596f76f3fe652c02664
Jason Urton <jason.urton@gmail.com>
2013-03-04 12:34:18 -0500

    nudge routes

--- app/controllers/askers_controller.rb
+++ app/controllers/askers_controller.rb
@@ -123,4 +123,14 @@ class AskersController < ApplicationController
     
     render :partial => params[:party]
   end
+
+  def nudge
+    asker = Asker.find(params[:asker_id])
+    user = User.find(params[:user_id])
+    nudge_type = NudgeType.find(params[:nudge_type_id])
+
+    nudge_type.send_to(asker, user)
+
+    render :nothing => true
+  end
 end
