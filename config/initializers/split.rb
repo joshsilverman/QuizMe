417f1fd7afcc84d09bd74974219fdcebab5efb83
Jason Urton <jason.urton@gmail.com>
2013-03-28 16:58:15 -0400

    misc

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,13 +1,13 @@
-# Split.configure do |config|
-#   config.db_failover = true # handle redis errors gracefully
-#   config.db_failover_on_db_error = proc{|error| Rails.logger.error(error.message) }
-#   config.allow_multiple_experiments = true
-#   config.enabled = true
-#   config.user_store = :redis_store
-#   config.robot_regex = /\b(Baidu|bot|BOT|Bot|butterfly|crawler|Crawler|expander|Gigabot|Googlebot|http|libwww-perl|lwp-trivial|msnbot|resolver|SiteUptime|Slurp|WordPress|ZIBB|ZyBorg)\b/i
-#   config.allowed_user_agent_regex = /Mozilla/
-# end
+Split.configure do |config|
+  config.db_failover = true # handle redis errors gracefully
+  config.db_failover_on_db_error = proc{|error| Rails.logger.error(error.message) }
+  config.allow_multiple_experiments = true
+  config.enabled = true
+  config.user_store = :redis_store
+  config.robot_regex = /\b(Baidu|bot|BOT|Bot|butterfly|crawler|Crawler|expander|Gigabot|Googlebot|http|libwww-perl|lwp-trivial|msnbot|resolver|SiteUptime|Slurp|WordPress|ZIBB|ZyBorg)\b/i
+  config.allowed_user_agent_regex = /Mozilla/
+end
 
-# if REDIS.present?
-# 	Split.redis = REDIS 
-# end
+if REDIS.present?
+	Split.redis = REDIS 
+end
