a6620c18f4894db97ba116e91a456ac654de404e
Jason Urton <jason.urton@gmail.com>
2013-03-30 15:16:01 -0400

    minor

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -7,6 +7,9 @@
 REDIS = nil
 puts Rails.env
 if ENV['REDISTOGO_URL'].present?
+	puts ENV['REDISTOGO_URL']
 	uri = URI.parse(ENV['REDISTOGO_URL'])
+	puts uri.to_json
 	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
+	puts REDIS.to_json
 end
