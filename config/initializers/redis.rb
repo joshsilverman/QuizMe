5318aa40a16e06e42286ffe67a65ef3782c47641
Jason Urton <jason.urton@gmail.com>
2013-03-30 15:31:42 -0400

    comment split methods for staging

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,11 +5,7 @@
 # 	uri = URI.parse('redis://localhost:6379')
 # end
 REDIS = nil
-puts Rails.env
 if ENV['REDISTOGO_URL'].present?
-	puts ENV['REDISTOGO_URL']
 	uri = URI.parse(ENV['REDISTOGO_URL'])
-	puts uri.to_json
 	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
-	puts REDIS.to_json
 end
