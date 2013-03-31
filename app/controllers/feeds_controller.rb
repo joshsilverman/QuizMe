cd865112745105603359a583c893c44a6417cdf0
Jason Urton <jason.urton@gmail.com>
2013-03-31 17:04:09 -0400

    remove cache invalidation for /respond_to_question

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -183,7 +183,7 @@ class FeedsController < ApplicationController
     user_post = current_user.app_answer(@question_asker, post, answer, { :conversation_id => @conversation.id, :in_reply_to_question_id => publication.question_id, :post_to_twitter => post_to_twitter })
     @question_asker.app_response(user_post, answer.correct, { :conversation_id => @conversation.id, :post_to_twitter => post_to_twitter, :link_to_parent => true }) if user_post
 
-    Rails.cache.delete "publications_recent_responses_by_asker_#{@question_asker.id}"
+    # Rails.cache.delete "publications_recent_responses_by_asker_#{@question_asker.id}"
 
     render :partial => "conversation"
   end
