fe8834dc0e1f8e0a26a59e3ce08406abbaee389a
Jason Urton <jason.urton@gmail.com>
2013-05-15 16:05:10 -0400

    comment out add + remove follow/follower methods

--- lib/adapters/twitter/manage_twitter_relationships.rb
+++ lib/adapters/twitter/manage_twitter_relationships.rb
@@ -56,8 +56,8 @@ module ManageTwitterRelationships
     twi_follows_ids = request_and_update_follows
     twi_follower_ids = request_and_update_followers
 
-    followback(twi_follower_ids) unless twi_follower_ids.blank?
-    unfollow_nonreciprocal(twi_follows_ids) unless twi_follows_ids.blank?
+    # followback(twi_follower_ids) unless twi_follower_ids.blank?
+    # unfollow_nonreciprocal(twi_follows_ids) unless twi_follows_ids.blank?
   end
 
   # FOLLOWS METHODS
@@ -85,20 +85,20 @@ module ManageTwitterRelationships
     nonreciprocal_follower_ids = [0] if nonreciprocal_follower_ids.empty?
     relationships.active.where('updated_at < ? AND followed_id IN (?)', limit, nonreciprocal_follower_ids).each do |nonreciprocal_relationship|
       puts "unfollow nonreciprocal user_id #{nonreciprocal_relationship.followed_id} on #{twi_screen_name}"
-      # user = User.find(nonreciprocal_relationship.followed_id)
-      # Post.twitter_request { twitter.unfollow(user.twi_user_id) }
-      # remove_follow(user)
+      user = User.find(nonreciprocal_relationship.followed_id)
+      Post.twitter_request { twitter.unfollow(user.twi_user_id) }
+      remove_follow(user)
     end
   end  
 
   def add_follow user, type_id = nil
-    relationship = Relationship.find_or_create_by_followed_id_and_follower_id(user.id, id)
-    relationship.update_attributes(active: true, type_id: type_id)
+    # relationship = Relationship.find_or_create_by_followed_id_and_follower_id(user.id, id)
+    # relationship.update_attributes(active: true, type_id: type_id)
   end
 
   def remove_follow user
-    relationship = Relationship.find_by_followed_id_and_follower_id(user.id, id)
-    relationship.update_attribute :active, false if relationship
+    # relationship = Relationship.find_by_followed_id_and_follower_id(user.id, id)
+    # relationship.update_attribute :active, false if relationship
   end  
 
   # FOLLOWER METHODS
@@ -115,7 +115,7 @@ module ManageTwitterRelationships
     (twi_follower_ids - wisr_follower_ids).each do |new_user_twi_id| 
       follower = User.find_or_create_by_twi_user_id(new_user_twi_id)
       follower_type_id = asker_follows.include?(follower) ? 1 : 3
-      # add_follower(follower, follower_type_id)
+      add_follower(follower, follower_type_id)
     end
 
     # Remove unfollowers from asker follow association    
@@ -127,23 +127,22 @@ module ManageTwitterRelationships
   def followback twi_follower_ids
     (twi_follower_ids - follows.collect(&:twi_user_id)).each do |twi_user_id|
       puts "followback follow twi_user_id #{twi_user_id} on #{twi_screen_name}"
-      # Post.twitter_request { twitter.follow(twi_user_id) }
-      # user = User.find_or_create_by_twi_user_id(twi_user_id)
-      # puts user.to_json
-      # add_follow(user, 1)
+      Post.twitter_request { twitter.follow(twi_user_id) }
+      user = User.find_or_create_by_twi_user_id(twi_user_id)
+      add_follow(user, 1)
     end
   end
 
   def add_follower user, type_id = nil
-    relationship = Relationship.find_or_create_by_followed_id_and_follower_id(id, user.id)
-    relationship.update_attributes(active: true, type_id: type_id)
-    send_new_user_question(user)
-    user.segment
+    # relationship = Relationship.find_or_create_by_followed_id_and_follower_id(id, user.id)
+    # relationship.update_attributes(active: true, type_id: type_id)
+    # send_new_user_question(user)
+    # user.segment
   end
 
   def remove_follower user
-    relationship = Relationship.find_by_followed_id_and_follower_id(id, user.id)
-    relationship.update_attribute :active, false if relationship
-    user.segment
+    # relationship = Relationship.find_by_followed_id_and_follower_id(id, user.id)
+    # relationship.update_attribute :active, false if relationship
+    # user.segment
   end
 end
