75d90ea43ade8f5bc88f051d7d35ac32685c8eb5
Josh Silverman <joshs.silverman@gmail.com>
2013-05-31 13:42:53 -0400

    capybara running with first few tests on moderations; some naming convention changes for test directories

--- test/models/asker_test.rb
+++ test/models/asker_test.rb
@@ -212,20 +212,12 @@ describe Asker do
 		    wisr_follower_ids = @asker.followers.collect(&:twi_user_id)
 		    twi_follower_ids = @asker.update_followers(twi_follower_ids, wisr_follower_ids)
 		    @asker.followback(twi_follower_ids)
-<<<<<<< HEAD
 		    @asker.reload.follow_relationships.where("followed_id = ?", @user.id).first.type_id.must_equal 1
-=======
-		    @asker.reload.relationships.where("followed_id = ?", @user.id).first.type_id.must_equal 1
->>>>>>> capybara running with first few tests on moderations; some naming convention changes for test directories
 			end
 
 			it 'follows new follower back with pending requests' do
 				@pending_user = FactoryGirl.create(:user, twi_user_id: 3)
-<<<<<<< HEAD
 				relationship = @asker.follow_relationships.find_or_create_by_followed_id(@pending_user.id)
-=======
-				relationship = @asker.relationships.find_or_create_by_followed_id(@pending_user.id)
->>>>>>> capybara running with first few tests on moderations; some naming convention changes for test directories
 				relationship.update_attribute :pending, true
 
 		    twi_follower_ids = [@pending_user.twi_user_id, @new_user.twi_user_id]
@@ -238,11 +230,7 @@ describe Asker do
 
 			it 'updates converted pending users to not pending' do
 				@pending_user = FactoryGirl.create(:user, twi_user_id: 3)
-<<<<<<< HEAD
 				relationship = @asker.follow_relationships.find_or_create_by_followed_id(@pending_user.id)
-=======
-				relationship = @asker.relationships.find_or_create_by_followed_id(@pending_user.id)
->>>>>>> capybara running with first few tests on moderations; some naming convention changes for test directories
 				relationship.update_attribute :pending, true
 
 		    relationship.reload.pending.must_equal true
@@ -262,11 +250,7 @@ describe Asker do
 		    @asker.update_followers(twi_follower_ids, wisr_follower_ids)
 		    @asker = @asker.reload
 		    @asker.followers.count.must_equal 0
-<<<<<<< HEAD
 		    @asker.follower_relationships.count.must_equal 1
-=======
-		    @asker.reverse_relationships.count.must_equal 1
->>>>>>> capybara running with first few tests on moderations; some naming convention changes for test directories
 			end
 		end
 
