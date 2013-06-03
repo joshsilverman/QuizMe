e54310d6cc508d04cb9551487774d8abe712e176
Jason Urton <jason.urton@gmail.com>
2013-06-03 16:09:08 -0400

    fix follow and follower with inactive associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,12 +30,12 @@ class User < ActiveRecord::Base
   # has_many :relationships, :foreign_key => :follower_id, :dependent => :destroy
   has_many :follow_relationships, :foreign_key => :follower_id, :class_name => 'Relationship', :dependent => :destroy
   has_many :follows, :through => :follow_relationships, :source => :followed, :conditions => ["relationships.active = ?", true]
-  has_many :follows_with_inactive, :through => :relationships, :source => :followed
+  has_many :follows_with_inactive, :through => :follow_relationships, :source => :followed
 
   # has_many :reverse_relationships, :foreign_key => :followed_id, :class_name => 'Relationship', :dependent => :destroy
   has_many :follower_relationships, :foreign_key => :followed_id, :class_name => 'Relationship', :dependent => :destroy
   has_many :followers, :through => :follower_relationships, :source => :follower, :conditions => ["relationships.active = ?", true]
-  has_many :followers_with_inactive, :through => :reverse_relationships, :source => :follower
+  has_many :followers_with_inactive, :through => :follower_relationships, :source => :follower
 
   has_many :exams
 
