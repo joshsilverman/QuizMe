b81635eedf582bbbdc984efaf65875a3fe1b6edd
Jason Urton <jason.urton@gmail.com>
2013-06-05 16:04:00 -0400

    move subscribe var definition outside of split test

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -28,14 +28,15 @@ class FeedsController < ApplicationController
           .where("publication_id in (?)", @publications.collect(&:id))\
           .order("created_at DESC")
 
-        # alternative to 'becomes': set 'follows' association to return Asker objects
+        @subscribed = Asker.includes(:related_askers).where("id in (?)", current_user.follows.collect(&:id))
+
         if Post.create_split_test(current_user.id, 'other feeds panel shows related askers (=> regular)', 'false', 'true') == 'false'
           @related = Asker.select([:id, :twi_name, :description, :twi_profile_img_url])\
             .where(:id => ACCOUNT_DATA.keys.sample(3)).all          
         else
-          @subscribed = Asker.includes(:related_askers).where("id in (?)", current_user.follows.collect(&:id))
           @related = @subscribed.collect {|a| a.related_askers }.flatten.uniq.reject {|a| @subscribed.include? a }.sample(3)
         end 
+        
         @responses = Conversation.where(:user_id => current_user.id, :post_id => posts.collect(&:id)).includes(:posts).group_by(&:publication_id)      
         @actions = Post.recent_activity_on_posts(posts, Publication.recent_responses(posts))
         
