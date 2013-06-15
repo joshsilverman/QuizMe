5ae7cd93c0a61aae6f7f7fdd07c919689fd0c064
Josh Silverman <joshs.silverman@gmail.com>
2013-06-15 18:44:28 -0400

    exclude current day from stat.daus_by_date and up unauth show cache to 15 mins

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -56,6 +56,7 @@ class Stat < ActiveRecord::Base
   def self.dau_ids_by_date domain = 30
     user_ids_by_date_raw = Post.social.not_us.not_spam\
       .where("created_at > ?", Date.today - (domain + 31).days)\
+      .where("created_at < ?", Date.today)\
       .select(["to_char(posts.created_at, 'YYYY-MM-DD')", "array_to_string(array_agg(user_id),',')"]).group("to_char(posts.created_at, 'YYYY-MM-DD')").all
 
     user_ids_by_date = {}
