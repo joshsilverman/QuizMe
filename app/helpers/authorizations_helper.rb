ad28a56fc854412f46437de724f6ba872236c247
Jason Urton <jason.urton@gmail.com>
2013-06-26 18:06:47 -0400

    move authenticated_link method to authorizations helper

--- app/helpers/authorizations_helper.rb
+++ app/helpers/authorizations_helper.rb
@@ -1,2 +1,9 @@
 module AuthorizationsHelper
+	def authenticated_link link, user, expires_at
+		url = URI(link)
+		params = Rack::Utils.parse_query(url.query)
+		user.update_attribute :authentication_token, user.reset_authentication_token
+		params[:auth] = Base64.encode64({authentication_token: user.authentication_token, expires_at: expires_at.to_i}.to_query)
+		"#{url.path}?#{params.to_query}"
+	end		
 end
--- app/models/users/asker.rb
+++ app/models/users/asker.rb
@@ -1,6 +1,6 @@
 class Asker < User
   include ManageTwitterRelationships
-  include ApplicationHelper
+  include AuthorizationsHelper
 
   belongs_to :client
   has_many :questions, :foreign_key => :created_for_asker_id
