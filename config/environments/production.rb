a39f6295c0e07173e2e0beb0af225242ea4949eb
Jason Urton <jason.urton@gmail.com>
2013-07-16 14:49:40 -0400

    init assets on precompile => false in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,8 @@ Quizmemanager::Application.configure do
 
   config.eager_load = true
 
+  config.assets.initialize_on_precompile = false
+  
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
