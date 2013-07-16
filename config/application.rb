697218027bf282cdda0968b3fd4d972f6ddfc9a9
Jason Urton <jason.urton@gmail.com>
2013-07-16 14:38:31 -0400

    serve static assets => true

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,7 @@ module Quizmemanager
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.serve_static_assets = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
