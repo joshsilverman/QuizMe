9f7a0c40bdda76590279bcb518fd20e2af80ef14
Jason Urton <jason.urton@gmail.com>
2013-08-22 16:52:37 -0400

    debugs for autorespond

--- app/controllers/email_askers_controller.rb
+++ app/controllers/email_askers_controller.rb
@@ -3,12 +3,6 @@ class EmailAskersController < ApplicationController
 	skip_before_filter :referrer_data, :split_user
 
   def save_private_response
-    puts 'in save_private_response'
-    puts params.to_json
-    puts params['text']
-    # puts params['html']
-    # puts params['html'].force_encoding('UTF-8').encode('UTF-8')
-    # puts params['text'].force_encoding('UTF-8').encode('UTF-8')
     params['text'] = params['text'].encode('utf-8', 'iso-8859-1')
   	handle =  Mail::Address.new(params[:to]).local
     user = User.find_by_email Mail::Address.new(params[:from]).address
