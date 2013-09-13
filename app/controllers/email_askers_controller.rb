3ca47a9b2d19889c943793dafbcdd33dd6802d8c
Jason Urton <jason.urton@gmail.com>
2013-09-13 11:10:19 -0400

    debug for illegal char from emails

--- app/controllers/email_askers_controller.rb
+++ app/controllers/email_askers_controller.rb
@@ -3,6 +3,8 @@ class EmailAskersController < ApplicationController
 	skip_before_filter :referrer_data, :split_user
 
   def save_private_response
+    puts params['text']
+    puts params['text'].include?('�')
     params['text'] = params['text'].gsub('é', 'e').gsub('ó', 'o').gsub('á', 'a').gsub('í', 'i').gsub('å', 'a')
 
   	handle =  Mail::Address.new(params[:to]).local
