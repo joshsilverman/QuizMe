16d19761984885cea989366ca3ecd3aa5b4153e3
Josh Silverman <joshs.silverman@gmail.com>
2014-01-02 17:19:53 -0500

    fix bypass for nonexistent asker

--- db/migrate/20130924140537_create_askers_topics.rb
+++ db/migrate/20130924140537_create_askers_topics.rb
@@ -6,7 +6,7 @@ class CreateAskersTopics < ActiveRecord::Migration
     end
     
     Askertopic.all.each do |askertopic|
-      next if Asker.where(id: asker_id).empty?
+      next if Asker.where(askertopic.asker_id).empty?
 
       Asker.find(askertopic.asker_id).topics << Topic.find(askertopic.topic_id)
     end
