30e0a4323f185e7aa2330db22af48946102f088d
Josh Silverman <joshs.silverman@gmail.com>
2014-01-02 17:16:06 -0500

    whitespace

--- db/migrate/20130924140537_create_askers_topics.rb
+++ db/migrate/20130924140537_create_askers_topics.rb
@@ -7,8 +7,8 @@ class CreateAskersTopics < ActiveRecord::Migration
     
     Askertopic.all.each do |askertopic|
       next if Asker.where(id: asker_id).empty?
-      
-    	Asker.find(askertopic.asker_id).topics << Topic.find(askertopic.topic_id)
+
+      Asker.find(askertopic.asker_id).topics << Topic.find(askertopic.topic_id)
     end
   end	
 end
