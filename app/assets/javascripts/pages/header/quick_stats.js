7c2693c6ca62c35a286b8a80fe3518dae7d415ba
Josh Silverman <joshs.silverman@gmail.com>
2014-01-11 09:17:53 -0500

    smoother transition for quick-stats and DRYer code

--- app/assets/javascripts/pages/header/quick_stats.js
+++ app/assets/javascripts/pages/header/quick_stats.js
@@ -6,17 +6,17 @@ if ($('.quick-stats').length > 0) {
       quickStatsViewModel = new QuickStatsViewModel();
       ko.applyBindings(quickStatsViewModel, $('.quick-stats')[0]);
 
-      $.getJSON("/users/" + user_id + "/posts/answer_count", function(count) {
-        quickStatsViewModel.answer_count(count);
-      })
+      $.each(['.answer-count', '.moderation-count', '.question-count'], function(i, selector) {
+        var $this = $(selector),
+          dataSource = $this.attr('data-source'),
+          dataTarget = $this.attr('data-target');
 
-      $.getJSON("/users/" + user_id + "/moderations/count", function(count) {
-        quickStatsViewModel.moderation_count(count);
-      })
-
-      $.getJSON("/users/" + user_id + "/questions/count", function(count) {
-        quickStatsViewModel.question_count(count);
-      })
+        $.getJSON("/users/" + user_id + dataSource, function(count) {
+          quickStatsViewModel[dataTarget](count);
+          $this.addClass('visible');
+          $(selector + ' + .circle').addClass('visible');
+        })
+      });
 
       $('.quick-stat').tooltip();
     }
@@ -24,9 +24,9 @@ if ($('.quick-stats').length > 0) {
     function QuickStatsViewModel() {
       var self = this;
 
-      self.answer_count = ko.observable();
-      self.question_count = ko.observable();
-      self.moderation_count = ko.observable();
+      self.answerCount = ko.observable();
+      self.questionCount = ko.observable();
+      self.moderationCount = ko.observable();
     }
 
     init($('#current_user_id').val());
