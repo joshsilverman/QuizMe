4d9458be4f6082e5e960190ed8e4b1d28cb65caf
Josh Silverman <joshs.silverman@gmail.com>
2014-01-20 20:11:27 -0500

    request test for feeds index

--- test/controllers/feeds_controller_test.rb
+++ test/controllers/feeds_controller_test.rb
@@ -174,3 +174,10 @@ describe FeedsController do
 		end
 	end
 end
+
+describe FeedsController, "#index" do
+  it "responds with 200" do
+    get :index
+    response.status.must_equal 200
+  end
+end
