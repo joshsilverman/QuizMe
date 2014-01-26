3095f27dd6fa097ac0890d28377710c474464009
Josh Silverman <joshs.silverman@gmail.com>
2014-01-26 00:13:40 -0500

    allow auto respond to DM answers while not allowing duplicate grades

--- lib/engagement_engine/auto_respond.rb
+++ lib/engagement_engine/auto_respond.rb
@@ -5,7 +5,8 @@ module EngagementEngine::AutoRespond
     
     answerer = user_post.user  
     if user_post.is_dm?
-      return unless answerer.dm_conversation_history_with_asker(id).grade.blank?
+      
+      return if already_graded_dm? user_post, answerer
       return if user_post.is_moderatable? and rand <= 0.05
       
       interval = 210
@@ -14,9 +15,9 @@ module EngagementEngine::AutoRespond
         TwitterPrivateMessage.new(self, answerer, generate_response(
             user_post.autocorrect, 
             user_post.in_reply_to_question), 
-          { :in_reply_to_post_id => user_post.id, 
-            :intention => "dm autoresponse"}),
-        :run_at => interval.minutes.from_now)
+          { in_reply_to_post_id: user_post.id, 
+            intention: "grade"}),
+        run_at: interval.minutes.from_now)
 
       user_post.update_attribute :correct, user_post.autocorrect
       learner_level = "dm answer"
@@ -25,18 +26,35 @@ module EngagementEngine::AutoRespond
       return if user_post.is_moderatable? and rand <= 0.05
       root_post = user_post.conversation.post
       asker_response = app_response(user_post, user_post.autocorrect, {
-        :link_to_parent => false, 
-        :autoresponse => true,
-        :post_to_twitter => true,
-        :quote_user_answer => root_post.is_question_post? ? true : false,
-        :link_to_parent => root_post.is_question_post? ? false : true
+        link_to_parent: false, 
+        autoresponse: true,
+        post_to_twitter: true,
+        quote_user_answer: root_post.is_question_post? ? true : false,
+        link_to_parent: root_post.is_question_post? ? false : true
       })
-      conversation = user_post.conversation || Conversation.create(:publication_id => user_post.publication_id, :post_id => user_post.in_reply_to_post_id, :user_id => user_post.user_id)
+
+      conversation = user_post.conversation || Conversation.create(
+        publication_id: user_post.publication_id, 
+        post_id: user_post.in_reply_to_post_id, 
+        user_id: user_post.user_id)
+
       conversation.posts << user_post
       conversation.posts << asker_response
       learner_level = "twitter answer"
     end
     after_answer_filter(answerer, user_post, :learner_level => learner_level)
+  end
+
+  def already_graded_dm? user_post, answerer
+    question_post = user_post.parent
+    question  = user_post.in_reply_to_question
+
+    already_graded = answerer.posts.dms.where('created_at > ?', question_post.created_at)
+      .where(in_reply_to_question: question)
+      .where('correct IS NOT NULL')
+      .present?
+
+    return already_graded
   end
 
 end
--- test/lib/engagement_engine/auto_respond_test.rb
+++ test/lib/engagement_engine/auto_respond_test.rb
@@ -22,6 +22,7 @@ describe Asker, "EngagementEngine::AutoRespond#auto_respond" do
       in_reply_to_user_id: @asker.id, 
       interaction_type: 2, 
       in_reply_to_question_id: @question.id,
+      parent: create(:dm, created_at: 1.hour.ago),
       requires_action: true, 
       autocorrect: nil)
 
@@ -34,7 +35,7 @@ describe Asker, "EngagementEngine::AutoRespond#auto_respond" do
   end
 
   it 'automatically responds to autocorrected posts' do
-    @user_response.update_attributes(
+    @user_response.update(
       requires_action: true, 
       autocorrect: true)
 
@@ -46,8 +47,8 @@ describe Asker, "EngagementEngine::AutoRespond#auto_respond" do
       .count.must_equal 1
   end
 
-  it 'wont response to un-autocorrected posts' do
-    @user_response.update_attributes(
+  it 'wont respond to un-autocorrected posts' do
+    @user_response.update(
       requires_action: true, 
       autocorrect: nil)
 
@@ -57,5 +58,65 @@ describe Asker, "EngagementEngine::AutoRespond#auto_respond" do
         intention: 'grade', 
         in_reply_to_post_id: @user_response.id)
       .count.must_equal 0
+  end
+
+  it 'automatically responds to dm answer' do
+    @user_response.update(
+      requires_action: true, 
+      autocorrect: true,
+      interaction_type: 4)
+
+    @asker.auto_respond(@user_response)
+    @user_response.reload.requires_action.must_equal false
+    @asker.posts.where(
+        intention: 'grade', 
+        in_reply_to_post_id: @user_response.id)
+      .count.must_equal 1
+  end
+
+  it 'wont respond multiple times with grade to the same dm answer' do
+    @user_response.update(
+      requires_action: true, 
+      autocorrect: true,
+      interaction_type: 4)
+
+    @asker.auto_respond(@user_response)
+
+    @conversation.posts << @user_response_2 = create(:post, 
+      text: 'the correct answer again, dawg', 
+      user_id: @user.id, 
+      in_reply_to_user: @asker, 
+      interaction_type: 4, 
+      in_reply_to_question: @question,
+      parent: @user_response.parent,
+      requires_action: true,
+      autocorrect: true)
+
+    @asker.auto_respond(@user_response_2)
+    
+    @asker.posts.where(intention: 'grade').count.must_equal 1
+  end
+
+  it 'responds to multiple questions through dm' do
+    @user_response.update(
+      requires_action: true, 
+      autocorrect: true,
+      interaction_type: 4)
+
+    @asker.auto_respond(@user_response)
+
+    @conversation.posts << @user_response = create(:post, 
+      text: 'the correct answer again, dawg', 
+      user_id: @user.id, 
+      in_reply_to_user_id: @asker.id, 
+      interaction_type: 4, 
+      in_reply_to_question: create(:question),
+      parent: create(:dm, created_at: 1.hour.ago),
+      requires_action: true,
+      autocorrect: true)
+
+    @asker.auto_respond(@user_response)
+    
+    @asker.posts.where(intention: 'grade').count.must_equal 2
   end
 end
