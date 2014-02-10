89d3659db40ff101325b5d888db7b6366ea6bc3a
Josh Silverman <joshs.silverman@gmail.com>
2014-02-10 18:04:02 -0500

    add isElementInViewport utility to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -68,6 +68,17 @@ function confirm_js(){
 	});
 }
 
+function isElementInViewport (el) {
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
 puts = function(o) {console.log(o)}
 
 js_check();
