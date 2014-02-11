3a1e7a4537855bcf597e5d060fb496066dfc1b0f
Josh Silverman <joshs.silverman@gmail.com>
2014-02-11 17:58:32 -0500

    feeds controller paired down and passing

--- test/controllers/feeds_controller_test.rb
+++ test/controllers/feeds_controller_test.rb
@@ -12,7 +12,7 @@ describe FeedsController do
 		post.click unless post[:class].split(' ').include?('active')
 		assert post.has_selector?('.bottom_border')
 		post.all('h3').first.click
-		assert post.has_selector?('.interactions')
+		assert post.has_selector?('h3.correct, h3.incorrect')
 
 		return post
 	end
@@ -24,7 +24,9 @@ describe FeedsController do
 		@asker.followers << @user	
 
 		@question = create(:question, created_for_asker_id: @asker.id, status: 1, user: @user)
-		@publication = create(:publication, question: @question, asker: @asker)
+		@publication = create(:publication, question: @question, asker: @asker, published: true)
+		@publication.update_question
+
 		@question_post = create(:post, user_id: @asker.id, interaction_type: 1, question: @question, publication: @publication)		
 	end
 
@@ -92,7 +94,7 @@ describe FeedsController do
 
 		describe 'user answers question' do
 			before :each do 
-				visit "/feeds/#{@asker.id}"
+				visit "/#{@asker.subject_url}"
 				answer_question
 			end
 			
@@ -104,78 +106,6 @@ describe FeedsController do
 			it 'responds to user post' do
 				grade_post = @asker.posts.where(intention: 'grade').first
 				grade_post.in_reply_to_user_id.must_equal @user.id
-			end
-		end
-
-		describe 'after answer requests' do
-			describe 'question moderation' do
-				it 'unless not requested publication' do
-					visit "/feeds/#{@asker.id}"
-					assert answer_question.has_no_selector?('.feedback')
-				end
-
-				it 'if requested publication' do
-					visit "/feeds/#{@asker.id}/#{@publication.id}"
-					assert answer_question.has_selector?('.feedback')
-				end
-
-				it 'unless question already has feedback' do
-					@question.update(needs_edits: true)
-					visit "/feeds/#{@asker.id}/#{@publication.id}"
-					assert answer_question.has_no_selector?('.feedback')
-				end
-
-				it 'if user hasnt already provided a moderation' do
-					create(:question_moderation, user_id: @user.id, question_id: @question.id, type_id: 7)
-					visit "/feeds/#{@asker.id}/#{@publication.id}"
-					assert answer_question.has_no_selector?('.feedback')
-				end
-			end
-
-			describe 'email address' do
-				before :each do 
-					@user.update(email: nil)
-				end
-				
-				it 'unless user has an email address' do
-					@user.update(email: 'jason@jason.jason')
-					visit "/feeds/#{@asker.id}"
-					assert answer_question.has_no_selector?('.request_email')					
-				end
-
-				it "if user doesn't have an email address" do
-					visit "/feeds/#{@asker.id}"
-					assert answer_question.has_selector?('.request_email')
-				end
-
-				it "once a month" do
-					3.times do |i|
-						visit "/feeds/#{@asker.id}"
-						answer_question(@question.id)
-						page.all('.after_answer').count.must_equal ((i + 1) % 2)
-						
-						Timecop.travel(Time.now + (2.5).weeks)
-						@question = create(:question, created_for_asker_id: @asker.id, status: 1, user: @user)
-						@publication = create(:publication, question: @question, asker: @asker)
-						@question_post = create(:post, user_id: @asker.id, interaction_type: 1, question: @question, publication: @publication)
-						Rails.cache.clear
-					end			
-				end
-
-				it "and updates user record properly" do
-					visit "/feeds/#{@asker.id}"
-					post = answer_question
-
-					fill_in 'email_input', with: "This is my email"
-					post.find('.request_email .btn').click
-					assert post.find('.request_email input').visible?
-					@user.reload.email.must_be_nil
-
-					fill_in 'email_input', with: "jason@jason.jason"
-					page.find('.request_email .btn').click
-					assert post.has_no_selector?('.request_email input')
-					@user.reload.email.must_equal "jason@jason.jason"
-				end
 			end
 		end
 	end
