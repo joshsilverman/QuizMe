9f9d600a5688bb80463c6ee5033f54a80b4cda6d
Josh Silverman <joshs.silverman@gmail.com>
2014-03-27 22:15:34 -0400

    clean up publication model

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -40,46 +40,6 @@ class Publication < ActiveRecord::Base
     save
   end
 
-  def self.recent_by_asker_and_id asker_id, id
-    Publication.published.where(id: id, asker_id: asker_id)\
-      .includes([:asker, :posts, :question => [:answers, :user]]).first
-  end
-
-  def self.recent_publication_posts publications
-    Rails.cache.fetch 'publications_posts_recent', :expires_in => 5.minutes do
-      Post.select([:id, :created_at, :publication_id])\
-        .where("publication_id in (?)", publications.collect(&:id))\
-        .order("created_at DESC")\
-        .to_a
-    end
-  end
-
-  def self.recent_publication_posts_by_asker asker, publications
-    Rails.cache.fetch "publications_posts_recent_#{asker.id}", :expires_in => 5.minutes do
-      Post.select([:id, :created_at, :publication_id])\
-        .where("publication_id in (?)", publications.collect(&:id))\
-        .order("created_at DESC")\
-        .to_a
-    end
-  end  
-
-  def self.recent_responses posts
-    Rails.cache.fetch 'publications_recent_responses', :expires_in => 5.minutes do
-      Post.answers\
-        .select([:user_id, :interaction_type, :in_reply_to_post_id, :created_at])\
-        .where("in_reply_to_post_id in (?)", posts.collect(&:id))\
-        .order("created_at ASC")\
-        .includes(:user)\
-        .group_by(&:in_reply_to_post_id)
-    end
-  end
-
-  def self.published_count
-    Rails.cache.fetch('publications_published_count', :expires_in => 10.minutes) do
-      Publication.where(:published => true).count
-    end
-  end
-
   def self.recent offset = 0
     publications = Publication.published
       .where('first_posted_at IS NOT NULL')
