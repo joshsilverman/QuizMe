8af9b9873d47043e58f1487a54f4d2e15e7a63d2
Josh Silverman <joshs.silverman@gmail.com>
2014-04-05 18:39:31 -0400

    clean up old invalid lesson names

---
+++ db/migrate/20140405215427_data_fix_illegal_lesson_names.rb
@@ -0,0 +1,17 @@
+class DataFixIllegalLessonNames < ActiveRecord::Migration
+  def up
+    illegal_lesson_ids_names = [
+      [403, "Types of immune responses: Innate and Adaptive, Humoral vs Cell Mediated"], 
+      [382, "Hardy Weinberg Principle"], 
+      [394, "Oxidation and Reduction Review From Biological Point of View"], 
+      [358, "Chromosomes, Chromatids, Chromatin, etc"], 
+      [373, "Krebs, Citric Acid Cycle"], 
+      [396, "C4 Photosynthesis"], 
+      [393, "Sex Linked Traits"]]
+
+    illegal_lesson_ids_names.each do |id, name|
+      lesson = Topic.find(id)
+      lesson.update name: name
+    end
+  end
+end
