7ff7e2b907b700a1b2400ea5de2c994aed50f519
Josh Silverman <joshs.silverman@gmail.com>
2014-04-19 18:57:46 -0400

    ibid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419034140) do
+ActiveRecord::Schema.define(version: 20140419174358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140419034140) do
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "questionbase_id"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
