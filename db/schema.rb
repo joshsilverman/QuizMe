c74a53e9698ff5f3a618de4e17331413bbf697a4
Josh Silverman <joshs.silverman@gmail.com>
2014-04-25 08:56:38 -0400

    ibid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419235408) do
+ActiveRecord::Schema.define(version: 20140425124252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -303,6 +303,7 @@ ActiveRecord::Schema.define(version: 20140419235408) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "type_id"
+    t.integer  "questions_count", default: 0
   end
 
   create_table "transitions", force: true do |t|
