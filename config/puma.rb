6077d22833c00b423842b61a48c2405321ccbc7b
Josh Silverman <joshs.silverman@gmail.com>
2014-05-18 16:00:12 -0400

    disable puma worker killer

--- config/puma.rb
+++ config/puma.rb
@@ -14,10 +14,10 @@ on_worker_boot do
   end
 end
 
-PumaWorkerKiller.config do |config|
-  config.ram           = ENV['PUMA_RAM'] || 512
-  config.frequency     = 60*5
-  config.percent_usage = 0.95
-end
+# PumaWorkerKiller.config do |config|
+#   config.ram           = ENV['PUMA_RAM'] || 512
+#   config.frequency     = 60*5
+#   config.percent_usage = 0.95
+# end
 
-PumaWorkerKiller.start
+# PumaWorkerKiller.start
