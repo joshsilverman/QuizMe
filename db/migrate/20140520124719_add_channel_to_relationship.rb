a2df364a90c38233b0ae3e7da76c02fe0d504e5d
Josh Silverman <joshs.silverman@gmail.com>
2014-05-20 09:05:47 -0400

    all existing relationships are channel 0 (twitter)

--- db/migrate/20140520124719_add_channel_to_relationship.rb
+++ db/migrate/20140520124719_add_channel_to_relationship.rb
@@ -1,5 +1,11 @@
 class AddChannelToRelationship < ActiveRecord::Migration
-  def change
+  def up
     add_column :relationships, :channel, :integer
+
+    Relationship.update_all channel: 0
+  end
+
+  def down
+    remove_column :relationships, :channel
   end
 end
