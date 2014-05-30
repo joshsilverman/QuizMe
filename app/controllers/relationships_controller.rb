6939d743b785658ffd539587548a1cd6da1eb246
Josh Silverman <joshs.silverman@gmail.com>
2014-05-30 09:09:29 -0400

    restrict deactivation search to wisr relationships

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,7 @@
 class RelationshipsController < ApplicationController
   prepend_before_filter :check_for_authentication_token
   before_filter :authenticate_user!
-  protect_from_forgery except: [:create, :destroy]
+  protect_from_forgery except: [:create, :deactivate]
 
   def create
     relationship = Relationship.find_or_initialize_by({
@@ -19,10 +19,10 @@ class RelationshipsController < ApplicationController
   end
 
   def deactivate
-    relationship = Relationship
+    relationship = Relationship.wisr
       .where(followed_id: params[:followed_id])
       .where(follower_id: current_user.id).first
-
+    
     if relationship.try :wisr?
       relationship.active = false
       relationship.save
