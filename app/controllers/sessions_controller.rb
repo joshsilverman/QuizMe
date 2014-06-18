c06d0b439ef5857d5ebf244a487aa73df747d174
Josh Silverman <joshs.silverman@gmail.com>
2014-06-17 21:39:10 -0400

    sign in redirects working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,4 +26,11 @@ class SessionsController < Devise::SessionsController
       format.html.none {}
     end
   end
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
+    redirect_to after_sign_in_path_for(resource)
+  end
 end
