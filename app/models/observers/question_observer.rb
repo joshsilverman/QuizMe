f4df2811d7b352fd3eb8a32d142ce7ad53943b1b
Josh Silverman <joshs.silverman@gmail.com>
2014-07-06 18:13:30 -0400

    remove a/b test around requesting feedback when a new question is submitted

--- app/models/observers/question_observer.rb
+++ app/models/observers/question_observer.rb
@@ -1,10 +1,7 @@
 class QuestionObserver < ActiveRecord::Observer
   def after_create(question)
-    if Rails.env.test? or Post.create_split_test(question.user_id, "request immediate feedback on author's questions (return submission)", 'true', 'false') == 'true'
-      return if question.user_id.nil?
-
-      question.asker.delay.request_feedback_on_question(question)
-    end
+    return if question.user_id.nil?
+    question.asker.delay.request_feedback_on_question(question)
   end
 
   def after_save question
