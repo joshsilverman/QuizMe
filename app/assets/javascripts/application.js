4086c1f12827e2790e7a034f3dd11f597147c94f
Josh Silverman <joshs.silverman@gmail.com>
2014-07-12 17:17:27 -0400

    promote wisr app on iOS safari but not in-app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //= require lib/jquery.stellar
 //= require lib/knockout-3.0.0.min
 //= require lib/snap
+//= require lib/jQuery.smartWebBanner
 //= require lodash
 //= require moment
 
@@ -22,18 +23,19 @@
 //= require_tree .
 
 if (!window.console) console = {log: function() {}}
+puts = function(o) {console.log(o)}
 
 function DummyMixpanel() {
-	this.track = function() {};
-	this.track_pageview = function() {};
-	this.track_links = function() {};
-	this.track_forms = function() {};
-	this.register = function() {};
-	this.register_once = function() {};
-	this.unregister = function() {};
-	this.identify = function() {};
-	this.name_tag = function() {};
-	this.set_config = function() {};
+  this.track = function() {};
+  this.track_pageview = function() {};
+  this.track_links = function() {};
+  this.track_forms = function() {};
+  this.register = function() {};
+  this.register_once = function() {};
+  this.unregister = function() {};
+  this.identify = function() {};
+  this.name_tag = function() {};
+  this.set_config = function() {};
 }
 
 function readCookie(name) {
@@ -58,7 +60,24 @@ function isElementInViewport (el) {
     );
 }
 
-puts = function(o) {console.log(o)}
+setTimeout(
+  function() {
+    if ($('body.phone-variant').length > 0) return;
+
+    $().smartWebBanner({
+      title: "Wisr", // What the title of the "app" should be in the banner | Default: "Web App"
+      titleSwap: false, // Whether or not to use the title specified here has the default label of the home screen icon (otherwise uses the page's <title> tag) | Default: true
+      url: 'https://itunes.apple.com/us/app/wisr/id887180306', // URL to mask the page as before saving to home screen (allows for having it save the homepage of a site no matter what page the visitor is on) | Default: ''
+      author: "Wisr", // What the author of the "app" should be in the banner | Default: "Save to Home Screen"
+      speedIn: 0, // Show animation speed of the banner | Default: 300
+      speedOut: 0, // Close animation speed of the banner | Default: 400
+      daysHidden: 1, // Duration to hide the banner after being closed (0 = always show banner) | Default: 15
+      daysReminder: 1, // Duration to hide the banner after "Save" is clicked *separate from when the close button is clicked* (0 = always show banner) | Default: 90
+      useIcon: true, // Whether or not it should show site's apple touch icon (located via <link> tag) | Default: true
+      // debug: true,
+      iconOverwrite: "http://a5.mzstatic.com/us/r30/Purple/v4/fe/aa/81/feaa81a9-0821-d244-c577-5e386819380c/mzl.jihrktfn.175x175-75.jpg"
+  })}, 800
+)
 
 jQuery.timeago.settings.strings = {
    prefixAgo: "",
