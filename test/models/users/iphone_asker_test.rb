1cf74e5d61dbdab7e6f03e99b765823d3a3f1886
Josh Silverman <joshs.silverman@gmail.com>
2014-09-29 08:26:21 -0400

    update tests that rely on reengage inactive

--- test/models/users/iphone_asker_test.rb
+++ test/models/users/iphone_asker_test.rb
@@ -9,32 +9,33 @@ describe IphoneAsker do
   let(:iphoner) {create(:iphoner, twi_user_id: 1)}
   let(:non_iphoner) {create(:user)}
   let(:question) {create(:question, created_for_asker_id: asker.id, status: 1)}
+  let(:old_question) {create(:question, created_for_asker_id: asker.id, status: 1)}
   let(:strategy) {[1, 2, 4, 8]}
   let(:publication) {create(:publication, question_id: question.id)}
 
   before :each do
-    @question_status = create(:post, user_id: asker.id, interaction_type: 1, question_id: question.id, publication_id: publication.id)   
+    @question_status = create(:post, user_id: asker.id, interaction_type: 1, question_id: question.id, publication_id: publication.id)
 
-    @iphoner_response = create(:post, 
-        text: 'the correct answer, yo', 
-        user_id: iphoner.id, 
-        in_reply_to_user_id: asker.id, 
-        interaction_type: 2, 
-        in_reply_to_question_id: question.id)
+    @iphoner_response = create(:post,
+        text: 'the correct answer, yo',
+        user_id: iphoner.id,
+        in_reply_to_user_id: asker.id,
+        interaction_type: 2,
+        in_reply_to_question_id: old_question.id)
 
     @iphoner_response.update_attributes(
-        created_at: (strategy.first + 1).days.ago, 
+        created_at: (strategy.first + 1).days.ago,
         correct: true)
 
     iphoner.update_attributes(
-        last_answer_at: @iphoner_response.created_at, 
-        last_interaction_at: @iphoner_response.created_at, 
+        last_answer_at: @iphoner_response.created_at,
+        last_interaction_at: @iphoner_response.created_at,
         activity_segment: nil)
 
-    create(:post, 
-        in_reply_to_user_id: asker.id, 
-        correct: true, 
-        interaction_type: 2, 
+    create(:post,
+        in_reply_to_user_id: asker.id,
+        correct: true,
+        interaction_type: 2,
         in_reply_to_question_id: question.id)
 
     Delayed::Worker.delay_jobs = false
@@ -90,7 +91,7 @@ describe IphoneAsker do
       iphoner.update device_token: '<08a26d39 05745ffc f0e98d05 65ce8764 d03bae59 6f1f09f2 f4848a52 099b2f91>'
 
       APN.stubs :push
-      
+
       Asker.reengage_inactive_users strategy: strategy
       asker.posts.reengage_inactive.apns.where(in_reply_to_user_id: iphoner).count.must_equal 1
     end
@@ -100,7 +101,7 @@ describe IphoneAsker do
       iphoner.update device_token: nil
 
       APN.expects(:push).never
-      
+
       Asker.reengage_inactive_users strategy: strategy
       asker.posts.reengage_inactive.apns.where(in_reply_to_user_id: iphoner).count.must_equal 0
     end
@@ -111,19 +112,19 @@ describe IphoneAsker do
 
       Asker.reengage_inactive_users strategy: strategy
       asker.posts.reengage_inactive.where(in_reply_to_user_id: iphoner.reload).count.must_equal 1
-    end    
+    end
   end
 
   describe 'follows up on correct answer' do
     let(:publication) { create(:publication, question_id: question.id) }
 
-    before :each do 
+    before :each do
       @conversation = create(:conversation, publication_id: publication.id)
-      @user_response = create(:post, 
-          text: 'the incorrect answer', 
-          user_id: iphoner.id, 
-          in_reply_to_user_id: asker.id, 
-          interaction_type: 2, 
+      @user_response = create(:post,
+          text: 'the incorrect answer',
+          user_id: iphoner.id,
+          in_reply_to_user_id: asker.id,
+          interaction_type: 2,
           in_reply_to_question_id: question.id)
       @conversation.posts << @user_response
 
