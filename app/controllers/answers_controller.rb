6d8694ae6101f45626511fd6e7a51621e774c407
Josh Silverman <joshs.silverman@gmail.com>
2014-11-26 18:23:13 -0500

    more robust answers api

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,16 +1,30 @@
 class AnswersController < ApplicationController
-  before_filter :admin?, :except => [:update]
+  skip_before_filter :verify_authenticity_token, :only => [:update]
+  before_filter :authenticate_user!
 
   def update
-    @answer = Answer.find(params[:id])
-    @answer.question.update bad_answers: nil
-    @answer.question.update(status: 0) unless current_user.is_role? 'admin' or current_user.is_role? 'asker' 
-    redirect_to "/" unless @answer
-    
-    if @answer.update_attributes(params[:answer])
-      head :ok
+    answer = Answer.find(params[:id])
+    question = current_user.questions.where(id: answer.question_id).first
+    question ||= Question.find(answer.question_id) if current_user.is_role? "admin"
+    if !question
+      head :unauthorized
+      return
+    end
+
+    question.update bad_answers: nil
+    question.update(status: 0) unless current_user.is_role? 'admin' or current_user.is_role? 'asker' 
+    redirect_to "/" unless answer
+
+    # standardize params    
+    if params[:answer]
+      params[:answer].each { |k,v| params[k] = v }
+    end
+    safe_params = params.permit(:correct, :question_id, :text)
+
+    if answer.update(safe_params)
+      render json: answer
     else
-      render json: @answer.errors, status: :unprocessable_entity
+      render json: answer.errors, status: :unprocessable_entity
     end
   end
 end
--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 describe AnswersController, '#update' do
   let (:author) { create(:user, twi_user_id: 1, role: 'user') }
+  let (:admin) { create(:admin) }
+  let (:hacker) { create(:user) }
 
   let (:asker) { 
     a = create(:asker) 
@@ -18,5 +20,55 @@ describe AnswersController, '#update' do
     put :update, id: answer.id, answer: {text: "yoyos"}, format: :json
 
     question.reload.bad_answers.must_equal nil
+  end
+
+  it 'updates answer when edited with nested style' do
+    sign_in author
+    question.update!(bad_answers: true)
+    put :update, id: answer.id, answer: {text: "Star Wars I"}, format: :json
+
+    answer.reload.text.must_equal "Star Wars I"
+  end
+
+  it 'updates answer when edited with non-nested style' do
+    sign_in author
+    question.update!(bad_answers: true)
+    put :update, id: answer.id, text: "Star Wars I", format: :json
+
+    answer.reload.text.must_equal "Star Wars I"
+  end
+
+  it 'allows admin to edit another person question' do
+    sign_in admin
+    question.update!(bad_answers: true)
+    put :update, id: answer.id, text: "Star Wars I", format: :json
+
+    answer.reload.text.must_equal "Star Wars I"
+  end
+
+  it 'wont allow non-admin/non-author to edit' do
+    sign_in hacker
+    question.update!(bad_answers: true)
+    put :update, id: answer.id, text: "Star Wars I", format: :json
+
+    response.status.must_equal 401
+    answer.reload.text.wont_equal "Star Wars I"
+  end
+
+  it 'returns 401 if not authenticated' do
+    question.update!(bad_answers: true)
+    put :update, id: answer.id, text: "Star Wars I", format: :json
+
+    response.status.must_equal 401
+    answer.reload.text.wont_equal "Star Wars I"
+  end
+
+  it 'responds with answer' do
+    sign_in author
+    question.update!(bad_answers: true)
+    put :update, id: answer.id, answer: {text: "yoyos"}, format: :json
+
+    question.reload.bad_answers.must_equal nil
+    JSON.parse(response.body)['id'].to_i.must_equal answer.id
   end
 end
