ad002c8305d870b0c2c914a9cd027f72ae8e1cd9
Josh Silverman <joshs.silverman@gmail.com>
2014-12-15 08:48:34 -0500

    convert tab to spaces

--- app/models/topics/topic.rb
+++ app/models/topics/topic.rb
@@ -1,17 +1,17 @@
 class Topic < ActiveRecord::Base
-	has_many :search_term_users, foreign_key: :search_term_topic_id, class_name: 'User'
+  has_many :search_term_users, foreign_key: :search_term_topic_id, class_name: 'User'
 
   has_many :questions, -> { uniq }, through: :questions_topics, :dependent => :destroy
   has_many :questions_topics, :dependent => :destroy
 
-	has_and_belongs_to_many :askers, -> { uniq }, join_table: :askers_topics
+  has_and_belongs_to_many :askers, -> { uniq }, join_table: :askers_topics
 
-	scope :descriptions, -> { where("type_id = 1") } # can be dropped into a sentence like: "Learn about ___." 
-	scope :hashtags, -> { where("type_id = 2") }
-	scope :search_terms, -> { where("type_id = 3") }
-	scope :categories, -> { where("type_id = 4") }
-	scope :courses, -> { where("type_id = 5") }
-	scope :lessons, -> { where("type_id = 6") }
+  scope :descriptions, -> { where("type_id = 1") } # can be dropped into a sentence like: "Learn about ___." 
+  scope :hashtags, -> { where("type_id = 2") }
+  scope :search_terms, -> { where("type_id = 3") }
+  scope :categories, -> { where("type_id = 4") }
+  scope :courses, -> { where("type_id = 5") }
+  scope :lessons, -> { where("type_id = 6") }
   scope :published, -> { where("published = true") }
   scope :unpublished, -> { where("published <> true OR published IS NULL") }
 
@@ -19,25 +19,25 @@ class Topic < ActiveRecord::Base
   validates :name, uniqueness: true, if: Proc.new { |t| t.published }
   validates :name, presence: true
 
-	def lessons # returns a course's lessons
-		return nil unless type_id == 5
-		questions.includes(:topics).collect { |q| q.topics.select { |t| t.type_id == 6 } }.flatten.uniq
-	end	
+  def lessons # returns a course's lessons
+    return nil unless type_id == 5
+    questions.includes(:topics).collect { |q| q.topics.select { |t| t.type_id == 6 } }.flatten.uniq
+  end  
 
-	def courses # returns courses to which a lesson belongs
-		return nil unless type_id == 6
-		questions.includes(:topics).collect { |q| q.topics.select { |t| t.type_id == 5 } }.flatten.uniq
-	end
+  def courses # returns courses to which a lesson belongs
+    return nil unless type_id == 6
+    questions.includes(:topics).collect { |q| q.topics.select { |t| t.type_id == 5 } }.flatten.uniq
+  end
 
-	# intended only for use with lesson/course/category types
-	def percentage_completed_by_user user
-		question_ids = questions.collect &:id
-		return 1.0 if question_ids.count == 0
-		
-		question_count = question_ids.count
-		correctly_answered_questions = Question.joins(:in_reply_to_posts).where('questions.id' => question_ids).where('posts.user_id' => user.id, 'posts.correct' => true).group('questions.id')
-		correctly_answered_questions.to_a.count / question_count.to_f
-	end
+  # intended only for use with lesson/course/category types
+  def percentage_completed_by_user user
+    question_ids = questions.collect &:id
+    return 1.0 if question_ids.count == 0
+    
+    question_count = question_ids.count
+    correctly_answered_questions = Question.joins(:in_reply_to_posts).where('questions.id' => question_ids).where('posts.user_id' => user.id, 'posts.correct' => true).group('questions.id')
+    correctly_answered_questions.to_a.count / question_count.to_f
+  end
 
   def topic_url
     _name = name || ''
