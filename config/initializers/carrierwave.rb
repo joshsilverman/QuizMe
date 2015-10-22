ff43d9f23798c762e8038c7f96f56abe0b919403
Joshua Silverman <joshs.silverman@gmail.com>
2015-10-22 11:15:18 -0400

    remove deprecated pass/key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
   config.permissions = 0666
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIDQNX4IECM24XAEQ',
-    :aws_secret_access_key  => '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',
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
   }
   config.fog_directory  = 'wisr-sitemap'
 end
