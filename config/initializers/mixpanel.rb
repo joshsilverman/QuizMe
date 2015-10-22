220a88637f6d0c5efa83e1a513be108117d5a05a
Joshua Silverman <joshs.silverman@gmail.com>
2015-10-22 10:57:08 -0400

    remove key from repo

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,2 +1,2 @@
-MIXPANEL_KEY = (Rails.env.production? ? "cc41ff876080a580c5d9ca257d189162" : "e568e52157e29acc604c236e9ce4cfa6")
+MIXPANEL_KEY = ENV['MIXPANEL_KEY']
 MP = Mixpanel::Tracker.new MIXPANEL_KEY, {:async => true}
